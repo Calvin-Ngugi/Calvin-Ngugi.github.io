46b075ac6cc3f9b5a113242a9f1c0f9dab1c74bd
Calvin-Ngugi <ncalvin67@gmail.com>
2017-03-09 12:10:49 +0300

    initial commit

---
+++ error-handling.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP custom error-handling</title>
+</head>
+<body>
+	<?php
+	//basic error handling using the die()function
+	$pdf=fopen("file.pdf")
+
+	?>
+</body>
+</html>
